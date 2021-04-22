07a279d41eae561b0579c41efc4980ef4030cb0f
dozzler555 <80205889+dozzler555@users.noreply.github.com>
2021-04-23 02:53:37 +0530

    Oss

--- compile.sh
+++ compile.sh
@@ -1,47 +1,31 @@
 #!/bin/bash
+
+# Just a basic script U can improvise lateron asper ur need xD 
+
 mkdir -p /tmp/recovery
+
 cd /tmp/recovery
-apt install tmate -y
-apt install git -y
-git config --global user.name "Dazzler555"
-git config --global user.email "71560605+Dazzler555@users.noreply.github.com"
 
-tg(){
-	bot_api=1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U
-	your_telegram_id=$1
-	msg=$2
-	curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" --data "text=$msg&chat_id=${your_telegram_id}"
-}
+sudo apt install git -y
 
-id=1033360588
+repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_10.0 -g default,-device,-mips,-darwin,-notdefault
+ 
 
-tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "wercker starded :)" && tg $id "$send_shell"
+repo sync -j$(nproc --all)
 
-repo init --depth=1 -u  git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_10.0 -g default,-device,-mips,-darwin,-notdefault 
 
-repo sync -c -q --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
-
-git clone https://github.com/Dazzler555/rm.git -b android-10.0 device/xiaomi/RMX2020
+git clone https://github.com/Dazzler555/surtwr.git -b oss device/xiaomi/surya
 
 rm -rf out
-. build/envsetup.sh && lunch omni_RMX2020-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka -j$(nproc --all) recoveryimage
 
-cd out/target/product/violet
+. build/envsetup.sh && lunch omni_surya-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka recoveryimage
+
+# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
+
+cd out/target/product/surya
+
 curl -sL https://git.io/file-transfer | sh 
 
 ./transfer wet *.zip
+
 ./transfer wet recovery.img
-
-apt install wget -y
-
-wget https://raw.githubusercontent.com/tazz555/ntausjdohtajatis/main/rsfhsuwf.conf
-
-apt install python3-pip -y
-
-pip3 install telegram-send
-
-telegram-send --config rsfhsuwf.conf "Wreck:Job done...Sending zips"
-
-telegram-send --config rsfhsuwf.conf --file *.zip --caption "Build completed Successfully :)"
-
-telegram-send --config rsfhsuwf.conf "Wreck: Sent zips Succesfully"
